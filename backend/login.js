68695179a339c068130da7ba264950e0acfcccbc
DougSpam <ddougllasbezerra@gmail.com>
2024-04-03 22:23:39 -0300

    Atualização

---
+++ backend/login.js
@@ -0,0 +1,16 @@
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('usuario').value;
+    var password = document.getElementById('password').value;
+    if (username === 'Belteste09' && password === 'Belteste09') {
+        window.location.href = 'dashboard.html';
+    } else {
+        alert('Invalid username or password');
+    }
+}
+
+
+
+
+)
